a1661e56f92d4043a716e2f796bb44bd24f54d11
NgocHuong <gom.mxzi@gmail.com>
2016-08-25 11:14:49 +0700

    init repo

---
+++ 1.js
@@ -0,0 +1,10 @@
+﻿ $(function(){
+	/* var docao = $(window).height();
+	 $('.navbar-default').css({'height':docao})
+
+	 $(window).resize(function(){
+	 	var docao = $(window).height()
+	 $('.navbar-default').css({'height':docao})
+	 });*/
+})  
+ 
